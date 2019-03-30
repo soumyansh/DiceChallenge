11ad1161ea66103d3633bed6e69f5a5e4f877357
soumyansh <soumyansh@gmail.com>
2019-03-30 13:40:45 +0530

    final commit

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ if (randomNumber2==1) {
 }
 
 if(randomNumber1>randomNumber2){
-  document.querySelector("h1").innerHTML="Player 1 Akshat Wins";
+  document.querySelector("h1").innerHTML="Player 1 Wins";
 }else if(randomNumber2>randomNumber1){
-  document.querySelector("h1").innerHTML="Player 2 Ravi Wins";
+  document.querySelector("h1").innerHTML="Player 2 Wins";
 } else{
   document.querySelector("h1").innerHTML="It's a Draw";
 }
