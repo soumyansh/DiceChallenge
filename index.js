0777bcbdb3aed36d929e64b023e2ee4fa0ba0208
soumyansh <soumyansh@gmail.com>
2019-03-30 13:34:34 +0530

    30032019

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ if (randomNumber2==1) {
 }
 
 if(randomNumber1>randomNumber2){
-  document.querySelector("h1").innerHTML="Player 1 Wins";
+  document.querySelector("h1").innerHTML="Player 1 Akshat Wins";
 }else if(randomNumber2>randomNumber1){
-  document.querySelector("h1").innerHTML="Player 2 Wins";
+  document.querySelector("h1").innerHTML="Player 2 Ravi Wins";
 } else{
   document.querySelector("h1").innerHTML="It's a Draw";
 }
